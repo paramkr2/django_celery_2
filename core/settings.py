332db3e2185b857c9127547cebc368aa955eddc9
param71kr <param71kr@gmail.com>
2023-04-05 12:52:15 +0530

    increased ids

--- core/settings.py
+++ core/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-'''
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
