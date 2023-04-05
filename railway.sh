ec93cc005a5703cab3945f732141f7e7dba1b678
param71kr <param71kr@gmail.com>
2023-04-05 12:50:50 +0530

    increased ids

--- railway.sh
+++ railway.sh
@@ -2,5 +2,6 @@
 python manage.py migrate 
 python manage.py collectstatic 
 gunicorn core.wsgi --daemon
+python manage.py runscript load_data
 celery --app=core worker --loglevel=info --logfile=logs/celery.log -P eventlet
 celery flower -A core --port=5555 --broker=redis://default:xP6JtBIJS2MAPfoLMxZS@containers-us-west-48.railway.app:7317
