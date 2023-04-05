a818c0e92ab699855975c134c66e135034fd2e68
param71kr <param71kr@gmail.com>
2023-04-05 13:34:24 +0530

    increased ids

--- railway.sh
+++ railway.sh
@@ -3,6 +3,6 @@ python manage.py makemigrations
 python manage.py migrate 
 python manage.py collectstatic 
 gunicorn core.wsgi --daemon
-python manage.py runscript load_data
+
 celery --app=core worker --loglevel=info --logfile=logs/celery.log -P eventlet
-celery flower -A core --port=5555 --broker=redis://default:xP6JtBIJS2MAPfoLMxZS@containers-us-west-48.railway.app:7317
+#celery flower -A core --port=5555 --broker=redis://default:xP6JtBIJS2MAPfoLMxZS@containers-us-west-48.railway.app:7317
--- tasks/sample_tasks.py
+++ tasks/sample_tasks.py
@@ -2,10 +2,13 @@ import time
 
 from celery import shared_task
 from .generate_data import run
-
+from .load_data import load
 @shared_task
 def create_task(task_type):
 	print(f'Task Started')
-	path = run()
-	print(f'Task Ended')
-	return path 
+	if(int(task_type) == 1 ):
+		res = load()
+	else:
+		res = run()
+		
+	return res 
