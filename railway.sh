fe5affed5848ae492c4d766fcfca67e431ba4be0
paramkr2 <61884278+paramkr2@users.noreply.github.com>
2023-04-15 15:15:42 +0530

    Update railway.sh

--- railway.sh
+++ railway.sh
@@ -3,5 +3,5 @@ python manage.py makemigrations
 python manage.py migrate 
 python manage.py collectstatic 
 celery --app=core worker --loglevel=info  -P eventlet --uid=0 --gid=0 &
-gunicorn --log-leve=debug core.wsgi 
+gunicorn --log-level=debug core.wsgi 
 #celery flower -A core --port=5555 --broker=redis://default:xP6JtBIJS2MAPfoLMxZS@containers-us-west-48.railway.app:7317
