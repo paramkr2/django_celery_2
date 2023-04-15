0927794c3660191cf3c28309aa9dc3907a04c613
paramkr2 <61884278+paramkr2@users.noreply.github.com>
2023-04-15 15:35:14 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['djangocelery2-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+ALLOWED_HOSTS = ['djangocelery2-production.up.railway.app','django-celery.onrender.com', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app','https://django-celery.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
