752a6de76c47435d97ccde199dc13ed4c03f577c
paramkr2 <61884278+paramkr2@users.noreply.github.com>
2023-04-15 15:59:47 +0530

    reverse to db sqlite3
    Because the postgres is giving erron on time fielf, it cannot store time without the date

--- core/settings.py
+++ core/settings.py
@@ -80,12 +80,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway', 
-        'USER': 'postgres',
-        'PASSWORD': '63Qjq9jiJv6QwRx3IVv5',
-        'HOST': 'containers-us-west-62.railway.app', 
-        'PORT': '5752',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+
     }
 }
 
